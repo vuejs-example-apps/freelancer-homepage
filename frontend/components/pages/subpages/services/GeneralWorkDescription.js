84feb4304d8d05e46a7f74ea1d3696dab24017b5
Grigoriy Beziuk <gbezyuk@gmail.com>
2016-11-15 16:46:22 +0200

    subpages introduced; first for services

---
+++ frontend/components/pages/subpages/services/GeneralWorkDescription.js
@@ -0,0 +1,7 @@
+var GeneralWorkDescription = Vue.component('GeneralWorkDescription', function (resolve, reject) {
+    ajax.get("/components/pages/subpages/services/templates/GeneralWorkDescription.tpl.html", function (template_string) {
+        resolve({
+            template: template_string                
+        });
+    });
+});  
