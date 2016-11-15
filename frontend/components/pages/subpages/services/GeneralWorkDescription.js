71d395db4dd48bc189a495cb5546642c98234b3e
Grigoriy Beziuk <gbezyuk@gmail.com>
2016-11-15 18:36:12 +0200

    more details on services; data format updated as well

--- frontend/components/pages/subpages/services/GeneralWorkDescription.js
+++ frontend/components/pages/subpages/services/GeneralWorkDescription.js
@@ -2,7 +2,7 @@ var GeneralWorkDescription = Vue.component('GeneralWorkDescription', function (r
     ajax.get("/components/pages/subpages/services/templates/GeneralWorkDescription.tpl.html", function (template_string) {
         resolve({
             template: template_string,
-            props: ['stages']                
+            props: ['stages']
         });
     });
 });  
--- frontend/components/pages/subpages/services/ServicePackages.js
+++ frontend/components/pages/subpages/services/ServicePackages.js
@@ -2,7 +2,7 @@ var ServicePackages = Vue.component('ServicePackages', function (resolve, reject
     ajax.get("/components/pages/subpages/services/templates/ServicePackages.tpl.html", function (template_string) {
         resolve({
             template: template_string,
-            props: ['packages']                
+            props: ['packages', 'note']
         });
     });
 });  
