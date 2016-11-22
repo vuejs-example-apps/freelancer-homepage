be2729896e5a23c5223a59f51b1cbf28d216ceb3
Grigoriy Beziuk <gbezyuk@gmail.com>
2016-11-22 10:33:45 +0200

    Bio component updated to reflect API data

--- frontend/src/components/pages/Bio.vue
+++ frontend/src/components/pages/Bio.vue
@@ -1,12 +1,19 @@
 <template>
   <main>
-    <h2>BioPage</h2>    
+    <h2>{{ bio.title }}</h2>
+    <img :src="bio.image">
+    <p>{{ bio.text }}</p>    
   </main>
 </template>
 
 <script>
 export default {
-  name: 'bio-page'  
+  name: 'bio-page',
+  computed: {
+    'bio': function () {
+      return this.$store.state.texts.about_me;
+    }
+  }
 }
 </script>
 
